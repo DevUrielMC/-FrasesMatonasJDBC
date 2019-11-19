b2ae1279ed89d72fca9a9a753b9ce9da2f35a726
DevUrielMC <urimarcervantes.95@gmail.com>
2019-11-19 13:59:50 -0600

    Añadiendo la clase Connection

--- src/com/frasesmatonasjdbc/model/DatabaseUtil.java
+++ src/com/frasesmatonasjdbc/model/DatabaseUtil.java
@@ -1,5 +1,35 @@
 package com.frasesmatonasjdbc.model;
+import java.sql.Connection; //conection es una interfaz(define lo que se va a hacer pero no como lo hara)
+import java.sql.DriverManager;
 
 public class DatabaseUtil {
 
+	private static final String URL_FORMAT ="jdbc:%s://%s:%s/%s";
+	
+	
+	public static Connection getConnection()
+	{
+		String dbms="mysql";
+		String host="localhost";
+		String port="3306";
+		String databaseName="frases_matonas";
+		String user="root";
+		String password = "root";
+		String url = String.format(URL_FORMAT,  dbms, host,port,databaseName);
+		
+		
+		Connection connection= null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection= DriverManager.getConnection(url, user, password);
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		return connection;
+	}
+	
 }
