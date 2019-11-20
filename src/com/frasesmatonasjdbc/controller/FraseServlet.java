cbf2c8cdd9691d66cf826361d05830a95955ae82
DevUrielMC <urimarcervantes.95@gmail.com>
2019-11-20 10:20:39 -0600

    Se añaden los modelos de personas y el servlet de frases

--- src/com/frasesmatonasjdbc/controller/FraseServlet.java
+++ src/com/frasesmatonasjdbc/controller/FraseServlet.java
@@ -1,6 +1,5 @@
 package com.frasesmatonasjdbc.controller;
 
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,27 +8,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/PersonaServlet")
-public class PersonaServlet extends HttpServlet {
-
+@WebServlet("/FraseServlet")
+public class FraseServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PersonaServlet() {
+    public FraseServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		String txtNombre = req.getParameter("txtNombre");
-		String txtEdad = req.getParameter("txtEdad");
-		String txtCarrera = req.getParameter("txtCarrera");
-		//String txtFecha = req.getParameter("txtFecha");
+		
+		String txtContenido =req.getParameter("txtContenido");
+		String txtCandidad =req.getParameter("txtCandidad");
+		String txtFechaDesde =req.getParameter("txtFechaDesde");
+		
 	}
 
 }
---
+++ src/com/frasesmatonasjdbc/controller/Frases.java
@@ -0,0 +1,36 @@
+package com.frasesmatonasjdbc.controller;
+
+import java.sql.Date;
+
+public class Frases {
+
+	private String contenido;
+	private int cantidad;
+	private String fechaDesde;
+	
+	
+	public String getContenido() {
+		return this.contenido;
+		
+	}
+	public void  setContenido(String contenido) {
+		this.contenido=contenido;
+	}
+	
+	public int getCantidad() {
+		return this.cantidad;
+		
+	}
+	public void  getCantidad(int cantidad) {
+		this.cantidad=cantidad;
+	}
+	
+	public String getFechaDesde() {
+		return this.fechaDesde;
+		
+	}
+	public void  setFechaDesde(String fechaDesde) {
+		this.fechaDesde=fechaDesde;
+	}
+	
+}
